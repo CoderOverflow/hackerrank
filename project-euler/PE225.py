0fc764d1cfc215263045668277bf314ad5dc8fed
CodeOverflow <this.is.lyd@gmail.com>
2019-01-14 16:58:46 +0800

    Update PE225.py

--- project-euler/PE225.py
+++ project-euler/PE225.py
@@ -1,17 +1,16 @@
-#Point 55
+#Point 72.5
 
 t1, t2, t3, k = map(int, input().split())
-
 def findsomething(t1,t2,t3,k):
-    #kdict = {}
+    kdict = {}
     count=0
-    n=1
+    n=7
     while count < k:
         a,b,c = t1,t2,t3
         kkset = set()
         d = 1
         n += 2
-        '''
+
         for seed in kdict:
             if not n % kdict[seed]:
                 count += 1
@@ -19,13 +18,11 @@ def findsomething(t1,t2,t3,k):
                     return n
                 n += 2
                 break
-        '''
+
         while 1:
-            #d = (a + b + c) % n
-            #a,b,c = b,c,d
             key = (a, b, c)
             if key in kkset:
-                #kdict[k]=n
+                kdict[count]=n
                 count +=1
                 break
             else:
